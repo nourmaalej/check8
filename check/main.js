3c8c27d9fdf87a2905d9f6a563c1d6f2a70eecc6
nourmaalej <52163719+nourmaalej@users.noreply.github.com>
2019-07-01 16:17:56 +0200

    Add files via upload

---
+++ check/main.js
@@ -0,0 +1,56 @@
+function bold1()
+{
+var x=document.getElementById('text');
+   
+  if( x.style.fontWeight =='bold')
+  
+
+      x.style.fontWeight='normal';
+      else 
+      	 x.style.fontWeight='bold';
+
+  
+}
+     
+
+function italic1()
+{
+
+var x=document.getElementById('text');
+    if (x.style.fontStyle=='italic')
+    	x.style.fontStyle='normal';
+    else
+    	x.style.fontStyle='italic';
+ 
+}
+function line1()
+{
+
+var x=document.getElementById('text');
+    if (x.style.textDecoration =='underline')
+    	x.style.textDecoration='none';
+    else
+    	x.style.textDecoration ='underline';
+
+ 
+}
+function drop1()
+{
+var x=document.getElementById('text');
+var size=document.getElementById('sz').value;
+x.style.fontSize=size;
+
+}
+function drop2()
+{
+var x=document.getElementById('text');
+var style=document.getElementById('lz').value;
+x.style.fontFamily=style;
+}
+
+
+
+
+
+
+
